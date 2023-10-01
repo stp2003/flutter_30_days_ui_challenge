7b28a2ff62d651345dac95fb4700cccb8d826e90
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-01 10:23:05 +0530

    onboarding screen created

---
+++ lib/Day1_OnboardingScreens/screens/day_1_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Day1HomeScreen extends StatelessWidget {
+  const Day1HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Day 1'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
