3376cf52238ffbc4f0540c9cf950bf57d5d7dea1
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-04 12:35:05 +0530

    Day 4 -> category list added

---
+++ lib/Day4_FurnitureApp/screens/products/components/category_list.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_30_days_ui_challenge/Day4_FurnitureApp/utils/constants.dart';
+
+class CategoryList extends StatefulWidget {
+  const CategoryList({super.key});
+
+  @override
+  State<CategoryList> createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  int selectedIndex = 0;
+  List categories = ['All', 'Sofa', 'Park bench', 'Armchair'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      height: 30,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: index == categories.length - 1 ? kDefaultPadding : 0,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            decoration: BoxDecoration(
+              color: index == selectedIndex
+                  ? Colors.white.withOpacity(0.4)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Text(
+              categories[index],
+              style: const TextStyle(
+                color: Colors.white,
+                fontFamily: 'poppins-medium',
+                letterSpacing: 0.6,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
