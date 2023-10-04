b1696387207209a178d80d05823d9ec6d91bff50
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-04 12:35:28 +0530

    Day 4 -> constants added

---
+++ lib/Day4_FurnitureApp/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFD8C9D5);
+const kPrimaryColor = Color(0xFF03A662);
+const kSecondaryColor = Color(0xFFEAB05E);
+const kTextColor = Color(0xFF000839);
+const kTextLightColor = Color(0xFF726C6C);
+const kBlueColor = Color(0xFF1D9EBC);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 15),
+  blurRadius: 27,
+  color: Colors.black12,
+);
