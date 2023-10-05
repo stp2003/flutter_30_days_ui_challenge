fecae9c13eed5b039168d75cffaa24f5f9402f54
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-05 12:55:27 +0530

    Day 5 -> featured plants added

---
+++ lib/Day5_PlantApp/screens/home/components/featured_plants.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants.dart';
+
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  final String image;
+  final Function() press;
+
+  const FeaturePlantCard({
+    super.key,
+    required this.image,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
+  }
+}
