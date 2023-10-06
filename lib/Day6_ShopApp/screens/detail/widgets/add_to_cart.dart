9be7c470bf9977a8d0a2b41f50476123709d4719
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-06 15:57:56 +0530

    Day 6 -> add to cart added

---
+++ lib/Day6_ShopApp/screens/detail/widgets/add_to_cart.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_30_days_ui_challenge/Day6_ShopApp/models/product_info.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants/constants.dart';
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({super.key, required this.product});
+
+  final ProductInfo product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: kDefaultPadding),
+            height: 50,
+            width: 58,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              border: Border.all(
+                color: product.color,
+              ),
+            ),
+            child: IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/add_to_cart.svg",
+                colorFilter: ColorFilter.mode(product.color, BlendMode.srcIn),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 48),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                backgroundColor: product.color,
+              ),
+              child: Text(
+                "Buy  Now".toUpperCase(),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
