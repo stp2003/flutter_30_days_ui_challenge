4bbf33010ff8e20ca3077b1242ffaefb41f46dc1
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-06 15:56:50 +0530

    Day 6 -> cart counter added

---
+++ lib/Day6_ShopApp/screens/detail/widgets/cart_counter.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/constants.dart';
+
+class CartCounter extends StatefulWidget {
+  const CartCounter({super.key});
+
+  @override
+  State<CartCounter> createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        SizedBox(
+          width: 40,
+          height: 32,
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(13),
+              ),
+            ),
+            onPressed: () {
+              setState(() {
+                if (numOfItems > 1) {
+                  setState(() {
+                    numOfItems--;
+                  });
+                }
+              });
+            },
+            child: const Icon(Icons.remove),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+          child: Text(
+            numOfItems.toString().padLeft(2, "0"),
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        SizedBox(
+          width: 40,
+          height: 32,
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(13),
+              ),
+            ),
+            onPressed: () {
+              setState(() {
+                numOfItems++;
+              });
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
+    );
+  }
+}
