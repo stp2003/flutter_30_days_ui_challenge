66231dc5613c973fa737af4fd551fe9607967e57
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-07 11:24:06 +0530

    Day 7 and 8 -> models added

---
+++ lib/Day7_Day8__TravelApp/models/travel_info.dart
@@ -0,0 +1,71 @@
+final List<String> categories = [
+  'Hotel',
+  'Place',
+  'Flight',
+  'Food',
+];
+final List<String> hotelLink = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7cx_8GON1Hu55hkmD7O22QQ59OWNcZbb7Sg&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRCcjSmas3-f9AGt-q26_njtoQEzSS6vlT2Q&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC5xALlkGkL_PCxUyVt7x_kDTmgzuPP1Kgag&usqp=CAU',
+];
+
+final List<String> hotelName = [
+  'Santorini',
+  'Hotel Royal',
+  'LAX',
+];
+
+final List<String> hotelLocation = [
+  'Greece',
+  'Spain',
+  'USA',
+];
+
+final List<String> hotelAmount = [
+  '488',
+  '425',
+  '412',
+];
+
+final List<String> hotelRatings = [
+  '4.7',
+  '4.3',
+  '4.9',
+];
+
+final List<String> hotHotelRatings = [
+  '4.9',
+  '4.5',
+  '4.7',
+];
+
+final List<String> hotHotelLocation = [
+  'Indonesia',
+  'Japan',
+  'Mexico',
+];
+
+final List<String> hotHotelAmount = [
+  '580',
+  '540',
+  '480',
+];
+
+final List<String> hotHotelDiscount = [
+  '25',
+  '20',
+  '35',
+];
+
+final List<String> hotHotelName = [
+  'BaLi Motel Vung Tau',
+  'Tokyo Drift',
+  'Los Polos Hermanos',
+];
+
+final List<String> hotHotelLink = [
+  'https://indonesia.tripcanvas.co/wp-content/uploads/2020/08/Bali-pool-villa-feature.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC5xALlkGkL_PCxUyVt7x_kDTmgzuPP1Kgag&usqp=CAU',
+  'https://imgcap.capturetheatlas.com/wp-content/uploads/2019/08/best-villas-in-Bali.jpg',
+];
