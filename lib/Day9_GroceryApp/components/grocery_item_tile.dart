c080be100cbd153c4e011f412e7b257c112f3ff1
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-09 18:10:31 +0530

    Day 9 -> grocery item list added

---
+++ lib/Day9_GroceryApp/components/grocery_item_tile.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class GroceryItemTile extends StatelessWidget {
+  final String itemName;
+  final String itemPrice;
+  final String imagePath;
+  final color;
+  final void Function()? onPressed;
+
+  const GroceryItemTile({
+    super.key,
+    required this.itemName,
+    required this.itemPrice,
+    required this.imagePath,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: color[100],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Image.asset(
+                imagePath,
+                height: 64,
+              ),
+            ),
+            Text(
+              itemName,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            MaterialButton(
+              onPressed: onPressed,
+              color: color,
+              child: Text(
+                '\$$itemPrice',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
