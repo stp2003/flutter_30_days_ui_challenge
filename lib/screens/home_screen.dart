5851812ebb218cdef00dd6f2ec1d908a80e36fb9
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-11 11:12:58 +0530

    Day 11 -> routes and assets added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_30_days_ui_challenge/Day10_StopWatch/screens/day_10_home_screen.dart';
+import 'package:flutter_30_days_ui_challenge/Day11_SmartHome/screens/day_11_home_screen.dart';
 import 'package:flutter_30_days_ui_challenge/Day1_OnboardingScreens/screens/day_1_onboarding_screen.dart';
 import 'package:flutter_30_days_ui_challenge/Day2_LoginScreen/screens/day_2_login_screen.dart';
 import 'package:flutter_30_days_ui_challenge/Day3_SignUpScreen/screens/day_3_signup_screen.dart';
@@ -52,6 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
     const Day7and8HomeScreen(),
     const Day9IntroScreen(),
     const Day10HomeScreen(),
+    const Day11HomeScreen(),
   ];
 
   @override
