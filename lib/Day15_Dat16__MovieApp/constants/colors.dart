98c653bebae12ba4c8879392f81d9ca4c96f903a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-15 14:15:56 +0530

    Day 15 and 16 -> colors added

---
+++ lib/Day15_Dat16__MovieApp/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xff121012);
+const kButtonColor = Color.fromARGB(255, 89, 54, 133);
+const kSearchbarColor = Color(0xff382C3E);
