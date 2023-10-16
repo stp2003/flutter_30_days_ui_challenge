00f77d40ebb5e2a93ef041bf990ad47488c207ec
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-16 15:42:29 +0530

    Day 15 and 16 -> routes added added

---
+++ lib/Day15_Dat16__MovieApp/screens/day_15_and_16_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Day15And16DetailScreen extends StatefulWidget {
+  const Day15And16DetailScreen({super.key});
+
+  @override
+  State<Day15And16DetailScreen> createState() => _Day15And16DetailScreenState();
+}
+
+class _Day15And16DetailScreenState extends State<Day15And16DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/Day15_Dat16__MovieApp/widgets/custom_card.dart
+++ lib/Day15_Dat16__MovieApp/widgets/custom_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_30_days_ui_challenge/Day15_Dat16__MovieApp/models/movies.dart';
+import 'package:flutter_30_days_ui_challenge/Day15_Dat16__MovieApp/screens/day_15_and_16_detail_screen.dart';
 
 class CustomCard extends StatelessWidget {
   final MovieModel movieModel;
@@ -11,82 +12,92 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10.0),
-          height: 200.0,
-          width: 140.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                movieModel.imageAsset!,
-              ),
-              fit: BoxFit.cover,
-            ),
-            borderRadius: BorderRadius.circular(20.0),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Day15And16DetailScreen(),
           ),
-        ),
-        Positioned(
-          left: 15.0,
-          right: 15.0,
-          top: 208.0,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            height: 200.0,
+            width: 140.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  movieModel.imageAsset!,
+                ),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          Positioned(
+            left: 15.0,
+            right: 15.0,
+            top: 208.0,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movieModel.movieName!,
+                        style: const TextStyle(
+                          fontFamily: 'poppins_bold',
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      const SizedBox(height: 5.0),
+                      Text(
+                        movieModel.year!,
+                        style: const TextStyle(
+                          fontFamily: 'poppins_medium',
+                          color: Colors.white54,
+                          fontSize: 12.0,
+                          letterSpacing: 0.8,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      movieModel.movieName!,
-                      style: const TextStyle(
-                        fontFamily: 'poppins_bold',
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
-                    ),
-                    const SizedBox(height: 5.0),
-                    Text(
-                      movieModel.year!,
+                      movieModel.movieRating!,
+                      maxLines: 1,
+                      overflow: TextOverflow.clip,
                       style: const TextStyle(
                         fontFamily: 'poppins_medium',
                         color: Colors.white54,
-                        fontSize: 12.0,
+                        fontSize: 14.0,
                         letterSpacing: 0.8,
                       ),
                     ),
+                    const SizedBox(width: 5.0),
+                    const Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15.0,
+                    ),
+                    const SizedBox(width: 5.0),
                   ],
                 ),
-              ),
-              const SizedBox(width: 20.0),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    movieModel.movieRating!,
-                    maxLines: 1,
-                    overflow: TextOverflow.clip,
-                    style: const TextStyle(
-                      fontFamily: 'poppins_medium',
-                      color: Colors.white54,
-                      fontSize: 14.0,
-                      letterSpacing: 0.8,
-                    ),
-                  ),
-                  const SizedBox(width: 5.0),
-                  const Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                    size: 15.0,
-                  ),
-                  const SizedBox(width: 5.0),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
