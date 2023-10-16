9ec75da06816e7989bbfa739e389aae5960c7e5a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-16 18:34:41 +0530

    Day 15 and 16 -> detail screen comments added

--- lib/Day15_Dat16__MovieApp/screens/day_15_and_16_detail_screen.dart
+++ lib/Day15_Dat16__MovieApp/screens/day_15_and_16_detail_screen.dart
@@ -188,10 +188,95 @@ class _Day15And16DetailScreenState extends State<Day15And16DetailScreen> {
                           ],
                         ),
                       ),
+                      //??
+                      const SizedBox(height: 30.0),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10.0,
+                          horizontal: 10.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Comments',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'poppins_bold',
+                                    fontSize: 20.0,
+                                    letterSpacing: 0.8,
+                                  ),
+                                ),
+                                Text(
+                                  'See all',
+                                  style: TextStyle(
+                                    color: kButtonColor,
+                                    fontFamily: 'poppins_bold',
+                                    fontSize: 18.0,
+                                    letterSpacing: 0.8,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            //??
+                            buildCommentsCard(),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.15,
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
+            ),
+          ),
+          Positioned(
+            top: 40.0,
+            right: 20.0,
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white70,
+              ),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.cancel_outlined,
+                  color: kSearchbarColor,
+                  size: 20.0,
+                ),
+              ),
+            ),
+          ),
+
+          //??
+          Positioned(
+            bottom: 30.0,
+            left: 30.0,
+            right: 30.0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Container(
+                color: kButtonColor,
+                alignment: Alignment.center,
+                height: 70.0,
+                child: const Text(
+                  'Watch Movies',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'poppins_bold',
+                    fontSize: 18.0,
+                    letterSpacing: 0.8,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
@@ -216,6 +301,115 @@ class _Day15And16DetailScreenState extends State<Day15And16DetailScreen> {
           fontSize: 16.0,
           letterSpacing: 0.8,
         ),
+      ),
+    );
+  }
+
+  //??
+  Widget buildCommentsCard() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20.0),
+      height: 160.0,
+      child: ListView.builder(
+        itemCount: popularItems[0].comments!.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.only(right: 15.0),
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+            width: 300.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: kSearchbarColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 50.0,
+                          width: 50.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                popularItems[0]
+                                    .comments![index]['imageUrl']
+                                    .toString(),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              popularItems[0].comments![index]['name'],
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'poppins_bold',
+                                fontSize: 16.0,
+                                letterSpacing: 0.8,
+                              ),
+                            ),
+                            Text(
+                              popularItems[0].comments![index]['date'],
+                              style: const TextStyle(
+                                color: Colors.white38,
+                                fontFamily: 'poppins',
+                                letterSpacing: 0.8,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          popularItems[0].comments![index]['rating'],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'poppins_bold',
+                            letterSpacing: 0.8,
+                          ),
+                        ),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 13.0,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                //??
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: Text(
+                    popularItems[0].comments![index]['comment'],
+                    maxLines: 3,
+                    overflow: TextOverflow.clip,
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontFamily: 'poppins_medium',
+                      letterSpacing: 0.8,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
