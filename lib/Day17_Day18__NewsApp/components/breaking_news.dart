ee7cd9d4f2848b6439a9bb2e5d481ab6d99a1e9a
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-18 10:02:29 +0530

    Day 17 and 18 -> routes added

--- lib/Day17_Day18__NewsApp/components/breaking_news.dart
+++ lib/Day17_Day18__NewsApp/components/breaking_news.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_30_days_ui_challenge/Day17_Day18__NewsApp/model/article_model.dart';
+import 'package:flutter_30_days_ui_challenge/Day17_Day18__NewsApp/screens/day_17_and_18_article_screen.dart';
 import 'package:flutter_30_days_ui_challenge/Day17_Day18__NewsApp/widgets/image_container.dart';
 
 class BreakingNews extends StatelessWidget {
@@ -42,6 +43,33 @@ class BreakingNews extends StatelessWidget {
                   child: InkWell(
                     onTap: () {
                       //???
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const Day17And18ArticleScreen(),
+                          settings: RouteSettings(
+                            arguments: Article(
+                              id: '1',
+                              title:
+                                  'Lorem ipsum dolor sit amet, consectetur elit. Cras molestie maximus',
+                              subtitle:
+                                  'Aliquam laoreet ante non diam suscipit accumsan. Sed vel consequat leo, non suscipit odio. Aliquam turpis',
+                              body:
+                                  'Nullam sed augue a turpis bibendum cursus. Suspendisse potenti. Praesent mi ligula, mollis quis elit ac, eleifend vestibulum ex. Nullam quis sodales tellus. Integer feugiat dolor et nisi semper luctus. Nulla egestas nec augue facilisis pharetra. Sed ultricies nibh a odio aliquam, eu imperdiet purus aliquam. Donec id ante nec',
+                              author: 'Anna G. Wright',
+                              authorImageUrl:
+                                  'https://images.unsplash.com/photo-1658786403875-ef4086b78196?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+                              category: 'Politics',
+                              views: 1204,
+                              imageUrl:
+                                  'https://images.unsplash.com/photo-1656106534627-0fef76c8b042?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+                              createdAt: DateTime.now().subtract(
+                                const Duration(hours: 5),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
                     },
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
