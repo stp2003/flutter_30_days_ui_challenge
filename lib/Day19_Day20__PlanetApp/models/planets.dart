f99d306941c9e86846e1b9b88f721b0b56b3ca9b
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-19 08:40:32 +0530

    Day 19 and 20 -> model added

---
+++ lib/Day19_Day20__PlanetApp/models/planets.dart
@@ -0,0 +1,71 @@
+class PlanetsModel {
+  final String? image;
+  final String? name;
+  final double? size;
+  final int? distanceFromSun;
+
+  PlanetsModel({
+    required this.size,
+    required this.distanceFromSun,
+    required this.image,
+    required this.name,
+  });
+}
+
+List<PlanetsModel> planets = [
+  PlanetsModel(
+    image: 'assets/images/mercury.png',
+    name: 'Mercury',
+    size: 4879,
+    distanceFromSun: 57900000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/venus.png',
+    name: 'Venus',
+    size: 12104,
+    distanceFromSun: 108200000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/earth.png',
+    name: 'Earth',
+    size: 12756,
+    distanceFromSun: 149600000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/mars.png',
+    name: 'Mars',
+    size: 6805,
+    distanceFromSun: 227900000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/jupiter.png',
+    name: 'Jupiter',
+    size: 142984,
+    distanceFromSun: 778300000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/saturn.png',
+    name: 'Saturn',
+    size: 120536,
+    distanceFromSun: 1426720000,
+  ),
+  PlanetsModel(
+      image: 'assets/images/uranus.png',
+      name: 'Uranus',
+      size: 51118,
+      distanceFromSun: 2870970000),
+  PlanetsModel(
+    image: 'assets/images/neptune.png',
+    name: 'Neptune',
+    size: 49528,
+    distanceFromSun: 4498250000,
+  ),
+  PlanetsModel(
+    image: 'assets/images/pluto.png',
+    name: 'Pluto',
+    size: 2376.6,
+    distanceFromSun: 5906376272,
+  ),
+];
+String astronomicalNews =
+    'Around the world, people have long gazed up at the stars and found meaning in them. The Renaissance polymath Nicolaus Copernicus once wrote, “Of all things visible, the highest is the heaven of the fixed stars.” Five centuries later, Kalpana Chawla, the first Indian woman in space, said, “When you look at the stars and the galaxy, you feel that you are not just from one particular piece of land, but from the solar system.”';
