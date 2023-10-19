43586d61fa0df33a2e02adbdf7cc1191a30facf4
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-19 08:40:16 +0530

    Day 19 and 20 -> colors added

---
+++ lib/Day19_Day20__PlanetApp/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const darkBlue = Color(0xFF022638);
+const blue = Color(0xFF0090CE);
+const white = Color(0xFFFFFFFF);
+const black = Color(0xFF000000);
+const transparent = Colors.transparent;
