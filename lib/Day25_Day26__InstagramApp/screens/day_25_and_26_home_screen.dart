e0fbb5092984525ef0abceccaba64f06ec117f22
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-25 12:10:27 +0530

    Day 25 and 26 -> home screen routes pages added

---
+++ lib/Day25_Day26__InstagramApp/screens/day_25_and_26_home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/pages/account.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/pages/home.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/pages/reels.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/pages/search.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/pages/shop.dart';
+
+class Day25And26HomeScreen extends StatefulWidget {
+  const Day25And26HomeScreen({super.key});
+
+  @override
+  State<Day25And26HomeScreen> createState() => _Day25And26HomeScreenState();
+}
+
+class _Day25And26HomeScreenState extends State<Day25And26HomeScreen> {
+  int _selectedIndex = 0;
+
+  //??
+  void _navBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //??
+  final List<Widget> _children = [
+    const Home(),
+    const Search(),
+    const Reels(),
+    const Shop(),
+    const Account(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        elevation: 8.0,
+        currentIndex: _selectedIndex,
+        onTap: _navBar,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.video_collection_sharp), label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
+        ],
+      ),
+    );
+  }
+}
