daf5e61ff7005790647cf000ab043cf6152cda57
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-25 12:27:08 +0530

    Day 25 and 26 -> home pages added

---
+++ lib/Day25_Day26__InstagramApp/pages/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_30_days_ui_challenge/Day25_Day26__InstagramApp/utils/bubble_stories.dart';
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  final List people = ['Shashwat', 'Arafat', 'Ved', 'Ankita', 'Tejas', 'Anni'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Instagram',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'poppins_bold',
+                letterSpacing: 0.8,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.pink,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(24.0),
+                  child: Icon(Icons.share),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 130.0,
+            child: ListView.builder(
+              itemCount: people.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return BubbleStories(
+                  text: people[index],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
