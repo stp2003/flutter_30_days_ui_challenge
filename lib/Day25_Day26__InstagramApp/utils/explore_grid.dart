83521f1afbbba75bdb6252daaa9a581d57dae480
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-26 10:17:15 +0530

    Day 25 and 26 -> explore grid added

---
+++ lib/Day25_Day26__InstagramApp/utils/explore_grid.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ExploreGrid extends StatelessWidget {
+  const ExploreGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 20,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(2.5),
+          child: Container(
+            color: Colors.deepPurple[100],
+          ),
+        );
+      },
+    );
+  }
+}
