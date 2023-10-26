a5049f6947b1fc134213603c6789290b250de1a8
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-26 10:18:55 +0530

    Day 25 and 26 -> reels page added

---
+++ lib/Day25_Day26__InstagramApp/pages/reels.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Reels extends StatelessWidget {
+  const Reels({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Hello'));
+  }
+}
