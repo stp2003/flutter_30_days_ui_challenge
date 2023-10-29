5c43a570a6a5af44d19a6d7f62fd3e336cf3d9a5
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-29 11:41:33 +0530

    Day 29 and 30 -> image constants added

---
+++ lib/Day29_Day30__BatmanApp/constants/image_constants.dart
@@ -0,0 +1,14 @@
+class ImageConst {
+  static const String baseUrl = 'assets/images/';
+  static const String logo = '${baseUrl}logo_batman.png';
+  static const String batman = '${baseUrl}battinson.png';
+  static const String batmanBackground = '${baseUrl}batman_background.png';
+  static const String penguimBackground = '${baseUrl}penguim_background.jpg';
+  static const String theBatmanIntro = '${baseUrl}the_batman_intro.jpg';
+  static const String batLoad = '${baseUrl}batman.png';
+  static const String penguim = '${baseUrl}penguim.png';
+  static const String car1 = '${baseUrl}car_1.png';
+  static const String car2 = '${baseUrl}car_2.png';
+  static const String car3 = '${baseUrl}car_3.png';
+  static const String motorcycle = '${baseUrl}motorcycle.png';
+}
