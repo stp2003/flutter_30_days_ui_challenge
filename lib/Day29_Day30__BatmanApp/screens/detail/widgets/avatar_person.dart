cbccc47949c0937d0308a2f4145b707bb9738ce9
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-10-30 10:49:14 +0530

    Day 29 and 30 -> avatar person added

---
+++ lib/Day29_Day30__BatmanApp/screens/detail/widgets/avatar_person.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AvatarPerson extends StatelessWidget {
+  final String hero;
+
+  const AvatarPerson({
+    super.key,
+    required this.hero,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: hero == 'assets/images/penguim.png'
+          ? Get.height * 0.228
+          : Get.height * 0.21,
+      left: hero == 'assets/images/penguim.png'
+          ? Get.width * 0.17
+          : Get.width * 0.20,
+      child: Image.asset(
+        hero,
+        height: hero == 'assets/images/penguim.png'
+            ? Get.height * 0.28
+            : Get.height * 0.30,
+      ),
+    );
+  }
+}
